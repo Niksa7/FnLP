7bea53494a9f04df694541eed75564e5c37f0b18
Nikita Soroka <nikitasoroka228@gmail.com>
2022-03-31 23:52:07 +0300

    Первое задание is done

---
+++ LB5/Task11/Task11/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let test answer =
+    match answer with
+    | "F#" -> printfn "Подлиза"
+    | "Prolog" -> printfn "Подлиза"
+    | _ -> printfn "Неплохой выбор!"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Какой твой любимый язык программирования?"
+    let word = Console.ReadLine()
+    test word
+    0 
