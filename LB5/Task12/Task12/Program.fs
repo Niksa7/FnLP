0a709125152915db03313ee15a5c96002473799b
Nikita Soroka <nikitasoroka228@gmail.com>
2022-04-15 01:07:24 +0300

    Второе задание is done

---
+++ LB5/Task12/Task12/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+
+    let test answer =
+        match answer with
+        | "F#" | "Prolog" -> printfn "Подлиза"
+        | _ -> printfn "Неплохой выбор!"
+
+    printfn "Реализация композиции функций. \nКакой твой любимый язык программирования?"
+    (Console.ReadLine >> test >> Console.WriteLine)()
+
+    printfn "Реализация конвейера и каррирования. \nКакой твой любимый язык программирования?"
+    Console.ReadLine() |> test |> Console.WriteLine
+
+    0 
