4c97833508db15bdb6afb02219c921f975be5932
Nikita Soroka <nikitasoroka228@gmail.com>
2022-05-14 10:25:30 +0300

    Just Fibon

---
+++ LB5/TaskFibonacci/TaskFibonacci/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let rec fib n =
+    match n with
+    |1 |2 -> 1
+    |_ -> fib (n-1) + fib(n-2)
+
+let fib2 n =
+    let rec loop acc1 acc2 n =
+        match n with
+        | 0 -> acc1
+        | 1 -> acc2
+        | _ ->
+            loop acc2 (acc1 + acc2) (n - 1)
+    loop 0 1 n
